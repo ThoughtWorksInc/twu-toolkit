a5582aa74b7524d1bc6885bf73b198ad5ddb37bc
Duda Dornelles <dudassdornelles@gmail.com>
2013-08-26 11:25:44 +0530

    updating readme and some stuff

--- create_calendar.rb
+++ create_calendar.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 require_relative 'lib/twu_calendar'
 
 events = parse_events
--- lib/twu_calendar.rb
+++ lib/twu_calendar.rb
@@ -1,4 +1,4 @@
-require_relative 'config'
+require_relative '../config'
 require 'csv'
 
 class Event
