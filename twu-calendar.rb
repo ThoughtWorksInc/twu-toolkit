c3dda2e93d8fecf2112594e32372fbae2039c2c8
Duda Dornelles <dudassdornelles@gmail.com>
2013-08-26 09:34:22 +0530

    refactoring

--- twu-calendar.rb
+++ twu-calendar.rb
@@ -1,26 +1,43 @@
-require './config'
+require_relative 'config'
 require 'csv'
 
-FILE = 'calendar-base.csv'
-OPTIONS = { :headers => true }
+class Event
+  attr_reader :month, :day, :time, :name, :presenters
+  def initialize(month, day, time, name, presenters) 
+    @month = month
+    @day = day
+    @time = time
+    @name = name
+    @presenters = presenters
+  end
 
-def create_event_name(month, day, time, name, presenters) 
-  event_name = "#{name} on #{month} #{day} at #{time}"
-  event_name += " - #{presenters}" if !presenters.nil?
-  event_name
+  def event_name
+    event_name = "#{name}" 
+    event_name += " - #{presenters}" if !presenters.nil?
+    event_name += " on #{month} #{day} #{time}"
+    event_name
+  end
+
 end
 
-CSV.foreach(FILE,  OPTIONS) do |row|
-  event_name = create_event_name(row[0], row[1], row[2], row[3], row[4])
-  command = "google calendar add --cal #{CALENDAR} \"#{event_name}\""
-  
-  5.times do
+def parse_events
+  events = []
+  CSV.foreach(FILE, { :headers => true }) do |row|
+    events << Event.new(row[0], row[1], row[2], row[3], row[4])
+  end
+  events
+end
+
+def create_calendar(events)
+  events.each do |event| 
+    command = "google calendar add --cal #{CALENDAR} \"#{event.event_name}\""
+    5.times do
       system(command)
-      if $?.exitstatus != 0 then
-        sleep 5
-      else
-        break
-      end
+      ($?.exitstatus != 0) ? sleep(5) : break
+    end
   end
 end
+
+events = parse_events
+create_calendar(events)
 
