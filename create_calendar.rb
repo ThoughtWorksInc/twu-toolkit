d68cf173cddc7dfdc845cd48b4958bb401019b2c
Duda Dornelles <dudassdornelles@gmail.com>
2013-09-23 14:09:56 +0530

    getting rid of old scripts

--- create_calendar.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-
-require_relative 'lib/twu_calendar'
-
-events = parse_events
-events.each { |e| puts e.calendar_name }
--- create_trello.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-
-require_relative 'lib/twu_calendar'
-
-events = parse_events
-create_trello_board(events)
