497c5d61a702c6caadc7bca618cf7ad3814321e8
Duda Dornelles <dudassdornelles@gmail.com>
2013-10-16 18:53:29 -0500

    revamping style

--- public/twu_toolkit.js
+++ public/twu_toolkit.js
@@ -3,6 +3,7 @@ $(function () {
     var button = $(element.target);
     button.addClass('button_loading');
     button.attr('disabled', 'disabled'); 
+    debugger;
     button.parent().parent().submit();
   });
 });
