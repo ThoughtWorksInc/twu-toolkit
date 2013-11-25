742efa535e976ee1d2c5c929a90532ee451286b0
Duda Dornelles <dudassdornelles@gmail.com>
2013-11-25 10:52:13 +0530

    adding client side validaton

--- public/twu_toolkit.js
+++ public/twu_toolkit.js
@@ -1,10 +1,4 @@
 $(function () {
-  $("input[type=submit]").click(function (element) {
-    var button = $(element.target);
-    button.addClass('button_loading');
-    button.attr('disabled', 'disabled'); 
-    button.parent().parent().submit();
-  });
 
   var notice = $(".notice");
   notice && notice.slideDown();
--- src/controllers/calendar_controller.rb
+++ src/controllers/calendar_controller.rb
@@ -1,26 +1,18 @@
 require 'models/events/event_parser'
 require 'services/google_calendar_service'
 require 'controllers/authorization/google'
-require 'controllers/viewmodels/calendar_data'
 
 module CalendarController
 
   def self.included(app)
 
     app.post '/create_calendar' do
-      @calendar_data = CalendarData.new(params)
-
-      if !@calendar_data.is_valid? then
-        flash[:warning] = "Some of the fields have invalid values. Please correct them and try again"
-        return erb "/calendar/calendar".to_sym
-      end 
-
       auth_code = session[:auth_code]
-      events = EventParser.new.parse_events(@calendar_data.calendar_csv, @calendar_data.start_date)
+      events = EventParser.new.parse_events(params[:calendar_csv], params[:start_date])
 
       begin
         cal = GoogleCalendarService.new(auth_code)
-        calendar_id = cal.create_calendar(@calendar_data.calendar_name)
+        calendar_id = cal.create_calendar(params[:calendar_name])
         cal.create_events(events, calendar_id)
         flash[:notice] = "Calendar succesfully create"
       rescue Exception => e
